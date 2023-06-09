7fec501f3d7a140aded6f113c602131eb9a4ee24
Monaliza Dasig <monalizadasig@gmail.com>
2023-06-09 09:26:13 -0700

    Add nav, logo and login btn to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,32 @@
+import { Flex, Button } from "@chakra-ui/react";
+import Logo from "./Logo";
+import Navigation from "./Navigation";
+
 function Header() {
-  return <h1>Hello World</h1>;
+  return (
+    <Flex
+      justifyContent='center'
+      alignItems='center'
+      bgColor='#FAECED'
+      p='20px'
+    >
+      <Flex w='85%' justifyContent='space-between' alignItems='center'>
+        <Logo />
+        <Navigation />
+        <Flex flex={1} justifyContent='flex-end'>
+          <Button
+            color='#fff'
+            bgColor='#FE7071'
+            p='0 30px'
+            borderRadius={30}
+            _hover={{ opacity: 0.8 }}
+          >
+            Login
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
 
 export default Header;
