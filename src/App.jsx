cd71a34ae4cc3b73ad388a23d09ca2bafedf1576
Monaliza Dasig <monalizadasig@gmail.com>
2023-07-05 13:26:32 -0700

    Add app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import { Flex } from "@chakra-ui/react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Recipes from "./components/Recipes";
-import ContactUs from "./components/ContactUs";
-import Hero from "./components/Hero";
+
+import Home from "./components/Home";
+import RecipeIndex from "./components/RecipeIndex";
+import AboutUs from "./components/AboutUs";
 import "./App.css";
 
 function App() {
   return (
     <Flex flexDir='column' justifyContent='space-between' h='100vh'>
       <Header />
-      <Hero />
-      <Flex
-        justifyContent='center'
-        alignItems='center'
-        margin={"15px 0px 25px 0px"}
-      >
-        <Recipes />
-      </Flex>
-      <Flex
-        justifyContent='center'
-        alignItems='center'
-        margin={"15px 0px 25px 0px"}
-      >
-        <ContactUs />
-      </Flex>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/recipe-index' element={<RecipeIndex />} />
+        <Route path='/about-us' element={<AboutUs />} />
+      </Routes>
       <Footer />
     </Flex>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,39 @@
 import { Flex, Text } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
     <Flex flex='10%' justifyContent='space-evenly'>
-      <Text fontSize='md' fontWeight='semibold'>
-        Home
-      </Text>
-      <Text fontSize='md' fontWeight='semibold'>
-        Recipes
-      </Text>
-      <Text fontSize='md' fontWeight='semibold'>
-        About Us
-      </Text>
+      <NavLink
+        to='/'
+        style={({ isActive }) => ({
+          color: isActive ? "#ff6a6a" : "unset",
+        })}
+      >
+        <Text fontSize='md' fontWeight='semibold'>
+          Home
+        </Text>
+      </NavLink>
+      <NavLink
+        to='/recipe-index'
+        style={({ isActive }) => ({
+          color: isActive ? "#ff6a6a" : "unset",
+        })}
+      >
+        <Text fontSize='md' fontWeight='semibold'>
+          Recipes
+        </Text>
+      </NavLink>
+      <NavLink
+        to='/about-us'
+        style={({ isActive }) => ({
+          color: isActive ? "#ff6a6a" : "unset",
+        })}
+      >
+        <Text fontSize='md' fontWeight='semibold'>
+          About Us
+        </Text>
+      </NavLink>
     </Flex>
   );
 }
