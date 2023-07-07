107f107ce08a6240f32984274923aeaab96735e6
Monaliza Dasig <monalizadasig@gmail.com>
2023-07-06 21:53:41 -0700

    Update recipe card photos

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -2,11 +2,16 @@ import { Flex, Text } from "@chakra-ui/react";
 
 function RecipeCard({ name, imgUrl }) {
   return (
-    <Flex flexDirection='column' alignItems='center' mb='20px'>
+    <Flex
+      flexDirection='column'
+      alignItems='center'
+      mb='20px'
+      style={{ objectFit: "cover" }}
+    >
       <img
         src={imgUrl}
         alt='recipe_bowl'
-        style={{ width: "30%", height: "auto" }}
+        style={{ width: "350px", height: "300px" }}
       />
       <Text fontWeight={600}>{name}</Text>
     </Flex>
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,22 +1,22 @@
 import { Flex, Text } from "@chakra-ui/react";
 import RecipeCard from "./RecipeCard";
-import recipe_bowl from "../assets/recipe.png";
+import recipe_bowl from "../assets/bowl.png";
 
 const recipeData = [
   {
     id: 1,
     name: "Recipe 1",
-    imgUrl: recipe_bowl,
+    imgUrl: "https://source.unsplash.com/kcA-c3f_3FE",
   },
   {
     id: 2,
     name: "Recipe 2",
-    imgUrl: recipe_bowl,
+    imgUrl: "https://source.unsplash.com/zcUgjyqEwe8",
   },
   {
     id: 3,
     name: "Recipe 3",
-    imgUrl: recipe_bowl,
+    imgUrl: "https://source.unsplash.com/vIm26fn_QKg",
   },
 ];
 
@@ -29,17 +29,22 @@ function Recipes() {
       // bgColor='#FAECED'
       p='20px'
     >
-      <Flex flexDirection='row' alignItems='center' justifyContent='center'>
+      <Flex
+        flexDirection='row'
+        alignItems='center'
+        justifyContent='center'
+        mb='30px'
+      >
         <img
           src={recipe_bowl}
           alt='recipe_bowl'
           style={{ width: "5%", height: "auto" }}
         />
-        <Text fontWeight={600} mb='20px'>
+        <Text fontWeight={600} ml={2}>
           Try Out New Recipes
         </Text>
       </Flex>
-      <Flex w='80%' justifyContent='center'>
+      <Flex w='80%' justifyContent='center' gap={3}>
         {recipeData.map((item) => (
           <RecipeCard key={item.id} name={item.name} imgUrl={item.imgUrl} />
         ))}
