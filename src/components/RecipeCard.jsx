10b0df15168056aacc8aaab3310b101e034f9e49
Monaliza Dasig <monalizadasig@gmail.com>
2023-07-17 07:06:21 -0700

    Add favorite icons to recipe card for fav recipes

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,19 +1,35 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Image } from "@chakra-ui/react";
+import favorite from "/assets/active_favorite.png";
 
-function RecipeCard({ name, imgUrl }) {
+function RecipeCard({ name, imgUrl, isFavorite }) {
   return (
     <Flex flexDirection='column' alignItems='center' mb='20px'>
-      <img
-        src={imgUrl}
-        alt='recipe_bowl'
+      <Flex
         style={{
-          width: "300px",
-          height: "300px",
-          objectFit: "cover",
-          borderRadius: 20,
-          boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+          position: "relative",
         }}
-      />
+      >
+        {isFavorite && (
+          <Image
+            src={favorite}
+            w='30px'
+            h='30px'
+            style={{ position: "absolute", right: 20, top: 20 }}
+          />
+        )}
+        <img
+          src={imgUrl}
+          alt='recipe_bowl'
+          style={{
+            width: "300px",
+            height: "300px",
+            objectFit: "cover",
+            borderRadius: 20,
+            boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+          }}
+        />
+      </Flex>
+
       <Text fontWeight={600}>{name}</Text>
     </Flex>
   );
--- src/user/UserFavorites.jsx
+++ src/user/UserFavorites.jsx
@@ -1,10 +1,64 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import RecipeCard from "../components/RecipeCard";
+// import { useNavigate } from "react-router-dom";
+import ScrollContainer from "../components/ScrollContainer";
+
+const favRecipesMockupData = [
+  {
+    id: 1,
+    title: "Gyoza",
+    imgSrc: "https://source.unsplash.com/kcA-c3f_3FE",
+    description: "This is a description.",
+    ingredients: ["cabbage, meat, onion, garlic, salt, pepper"],
+    instructions: ["Mixed all ingredients. Wrap. Cook."],
+  },
+  {
+    id: 2,
+    title: "Gyoza",
+    imgSrc: "https://source.unsplash.com/kcA-c3f_3FE",
+    description: "This is a description.",
+    ingredients: ["cabbage, meat, onion, garlic, salt, pepper"],
+    instructions: ["Mixed all ingredients. Wrap. Cook."],
+  },
+  {
+    id: 3,
+    title: "Gyoza",
+    imgSrc: "https://source.unsplash.com/kcA-c3f_3FE",
+    description: "This is a description.",
+    ingredients: ["cabbage, meat, onion, garlic, salt, pepper"],
+    instructions: ["Mixed all ingredients. Wrap. Cook."],
+  },
+];
 
 function UserFavorites() {
+  const [recipes, setRecipes] = useState(favRecipesMockupData);
+  // const navigate = useNavigate();
+
+  console.log({ setRecipes });
+  useEffect(() => {
+    const newRecipes = recipes.map((item) => {
+      return { ...item, isFavorite: true };
+    });
+    setRecipes(newRecipes);
+  }, [recipes]);
+
   return (
-    <Flex>
-      <Heading>User Favorites Page</Heading>
-    </Flex>
+    <>
+      <ScrollContainer>
+        <Grid templateColumns='repeat(3, 1fr)' gap={10} w='100%'>
+          {recipes.map((item) => (
+            <GridItem key={item.id}>
+              <RecipeCard
+                name={item.title}
+                imgUrl={item.imgSrc}
+                isFavorite={item.isFavorite}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </ScrollContainer>
+    </>
   );
 }
 
