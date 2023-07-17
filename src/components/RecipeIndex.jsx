0b4b557cd19737d9e50e116b8e3cd866f75136c4
Mirna Garcia <mhannany@gmail.com>
2023-07-17 11:30:59 -0700

    Add recipe index content

--- src/components/RecipeIndex.jsx
+++ src/components/RecipeIndex.jsx
@@ -1,10 +1,34 @@
-import { Center } from "@chakra-ui/react";
+import RecipeCard from "./RecipeCard";
+import SearchBar from "./SearchBar";
+import { Grid, VStack } from "@chakra-ui/react";
+import { recipeData } from "../data/RecipeData";
+import { useState } from "react";
 
 function RecipeIndex() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearch = (input) => {
+    setSearchInput(input);
+  };
+
+  const filteredRecipes = recipeData.filter((item) =>
+    item.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
-    <Center bg='tomato' h='100vh' color='white'>
-      This is Recipe Index Page
-    </Center>
+    <VStack>
+      <SearchBar handleSearch={handleSearch} />
+      <Grid templateColumns={"repeat(3, 1fr)"} gap={50} margin={10}>
+        {filteredRecipes.map((data) => (
+          <RecipeCard
+            key={data.id}
+            name={data.name}
+            imgUrl={data.imgUrl}
+            search={searchInput}
+          />
+        ))}
+      </Grid>
+    </VStack>
   );
 }
 
---
+++ src/data/RecipeData.jsx
@@ -0,0 +1,47 @@
+export const recipeData = [
+  {
+    id: 1,
+    name: "Recipe 1",
+    imgUrl: "https://source.unsplash.com/kcA-c3f_3FE",
+  },
+  {
+    id: 2,
+    name: "Recipe 2",
+    imgUrl: "https://source.unsplash.com/zcUgjyqEwe8",
+  },
+  {
+    id: 3,
+    name: "Recipe 3",
+    imgUrl: "https://source.unsplash.com/vIm26fn_QKg",
+  },
+  {
+    id: 4,
+    name: "Sample Recipe 4",
+    imgUrl: "https://source.unsplash.com/kcA-c3f_3FE",
+  },
+  {
+    id: 5,
+    name: "Sample Recipe 5",
+    imgUrl: "https://source.unsplash.com/zcUgjyqEwe8",
+  },
+  {
+    id: 6,
+    name: "Sample Recipe 6",
+    imgUrl: "https://source.unsplash.com/vIm26fn_QKg",
+  },
+  {
+    id: 7,
+    name: "Recipe 7",
+    imgUrl: "https://source.unsplash.com/kcA-c3f_3FE",
+  },
+  {
+    id: 8,
+    name: "Recipe 8",
+    imgUrl: "https://source.unsplash.com/zcUgjyqEwe8",
+  },
+  {
+    id: 9,
+    name: "Recipe 9",
+    imgUrl: "https://source.unsplash.com/vIm26fn_QKg",
+  },
+];
